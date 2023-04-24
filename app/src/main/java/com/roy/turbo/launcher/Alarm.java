8c8c8ecee8bc2c20cd40a2f2c9fce454089cc455
loitp <www.muathu@gmail.com>
2023-04-24 19:43:46 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/Alarm.java
+++ app/src/main/java/com/roy/turbo/launcher/Alarm.java
@@ -1,30 +1,14 @@
-/*
- * Copyright (C) 2010 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.roy.turbo.launcher;
 
 import android.os.Handler;
 
-public class Alarm implements Runnable{
-   
+public class Alarm implements Runnable {
+
     private long mAlarmTriggerTime;
 
     private boolean mWaitingForCallback;
 
-    private Handler mHandler;
+    private final Handler mHandler;
     private OnAlarmListener mAlarmListener;
     private boolean mAlarmPending = false;
 
