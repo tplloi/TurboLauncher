a944d45b0a8dd64f0d5803e179ff0b9d0953ebc0
loitp <www.muathu@gmail.com>
2023-04-24 19:52:37 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/AppInfo.kt
+++ app/src/main/java/com/roy/turbo/launcher/AppInfo.kt
@@ -1,116 +1,126 @@
-package com.roy.turbo.launcher;
+package com.roy.turbo.launcher
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.ResolveInfo;
-import android.graphics.Bitmap;
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.graphics.Bitmap
 
-import java.util.HashMap;
+internal class AppInfo : ItemInfo {
+    @JvmField
+    var intent: Intent? = null
 
-class AppInfo extends ItemInfo {
+    @JvmField
+    var iconBitmap: Bitmap? = null
 
-    Intent intent;
-    Bitmap iconBitmap;
-    long firstInstallTime;
+    @JvmField
+    var firstInstallTime: Long = 0
 
-    ComponentName componentName;
+    @JvmField
+    var componentName: ComponentName? = null
 
-    static final int DOWNLOADED_FLAG = 1;
-    static final int UPDATED_SYSTEM_APP_FLAG = 2;
+    @JvmField
+    var flags = 0
 
-    int flags = 0;
-
-    AppInfo() {
-        itemType = LauncherSettings.BaseLauncherColumns.ITEM_TYPE_SHORTCUT;
+    constructor() {
+        itemType = LauncherSettings.BaseLauncherColumns.ITEM_TYPE_SHORTCUT
     }
 
-    protected Intent getIntent() {
-        return intent;
+    public override fun getIntent(): Intent? {
+        return intent
     }
 
-    protected Intent getRestoredIntent() {
-        return null;
+    override fun getRestoredIntent(): Intent? {
+        return null
     }
 
     /**
      * Must not hold the Context.
      */
-    public AppInfo(PackageManager pm, ResolveInfo info, IconCache iconCache,
-                   HashMap<Object, CharSequence> labelCache) {
-        final String packageName = info.activityInfo.applicationInfo.packageName;
-
-        this.componentName = new ComponentName(packageName,
-                info.activityInfo.name);
-        this.container = ItemInfo.NO_ID;
-        this.setActivity(componentName, Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-
+    constructor(
+        pm: PackageManager, info: ResolveInfo, iconCache: IconCache,
+        labelCache: HashMap<Any?, CharSequence?>?
+    ) {
+        val packageName = info.activityInfo.applicationInfo.packageName
+        componentName = ComponentName(
+            packageName,
+            info.activityInfo.name
+        )
+        container = NO_ID.toLong()
+        setActivity(
+            componentName, Intent.FLAG_ACTIVITY_NEW_TASK
+                    or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+        )
         try {
-            PackageInfo pi = pm.getPackageInfo(packageName, 0);
-            flags = initFlags(pi);
-            firstInstallTime = initFirstInstallTime(pi);
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
+            val pi = pm.getPackageInfo(packageName, 0)
+            flags = initFlags(pi)
+            firstInstallTime = initFirstInstallTime(pi)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
-
-        iconCache.getTitleAndIcon(this, info, labelCache);
+        iconCache.getTitleAndIcon(this, info, labelCache)
     }
 
-    public static int initFlags(PackageInfo pi) {
-        int appFlags = pi.applicationInfo.flags;
-        int flags = 0;
-        if ((appFlags & android.content.pm.ApplicationInfo.FLAG_SYSTEM) == 0) {
-            flags |= DOWNLOADED_FLAG;
-
-            if ((appFlags & android.content.pm.ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
-                flags |= UPDATED_SYSTEM_APP_FLAG;
-            }
-        }
-        return flags;
-    }
-
-    public static long initFirstInstallTime(PackageInfo pi) {
-        return pi.firstInstallTime;
-    }
-
-    public AppInfo(AppInfo info) {
-        super(info);
-        componentName = info.componentName;
-        title = info.title.toString();
-        intent = new Intent(info.intent);
-        flags = info.flags;
-        firstInstallTime = info.firstInstallTime;
+    constructor(info: AppInfo) : super(info) {
+        componentName = info.componentName
+        title = info.title.toString()
+        intent = Intent(info.intent)
+        flags = info.flags
+        firstInstallTime = info.firstInstallTime
     }
 
     /**
      * Creates the application intent based on a component name and various
-     * launch flags. Sets {@link #itemType} to
-     * {@link LauncherSettings.BaseLauncherColumns#ITEM_TYPE_APPLICATION}.
+     * launch flags. Sets [.itemType] to
+     * [LauncherSettings.BaseLauncherColumns.ITEM_TYPE_APPLICATION].
      *
      * @param className   the class name of the component representing the intent
      * @param launchFlags the launch flags
      */
-    final void setActivity(ComponentName className, int launchFlags) {
-        intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        intent.setComponent(className);
-        intent.setFlags(launchFlags);
-        itemType = LauncherSettings.BaseLauncherColumns.ITEM_TYPE_APPLICATION;
+    fun setActivity(className: ComponentName?, launchFlags: Int) {
+        intent = Intent(Intent.ACTION_MAIN)
+        intent?.let {
+            it.addCategory(Intent.CATEGORY_LAUNCHER)
+            it.component = className
+            it.flags = launchFlags
+        }
+        itemType = LauncherSettings.BaseLauncherColumns.ITEM_TYPE_APPLICATION
     }
 
-    @Override
-    public String toString() {
-        return "ApplicationInfo(title=" + title.toString() + " id=" + this.id
-                + " type=" + this.itemType + " container=" + this.container
+    override fun toString(): String {
+        return ("ApplicationInfo(title=" + title.toString() + " id=" + id
+                + " type=" + itemType + " container=" + container
                 + " screen=" + screenId + " cellX=" + cellX + " cellY=" + cellY
                 + " spanX=" + spanX + " spanY=" + spanY + " dropPos=" + dropPos
-                + ")";
+                + ")")
     }
 
-    public ShortcutInfo makeShortcut() {
-        return new ShortcutInfo(this);
+    fun makeShortcut(): ShortcutInfo {
+        return ShortcutInfo(this)
+    }
+
+    companion object {
+        const val DOWNLOADED_FLAG = 1
+        private const val UPDATED_SYSTEM_APP_FLAG = 2
+
+        @JvmStatic
+        fun initFlags(pi: PackageInfo): Int {
+            val appFlags = pi.applicationInfo.flags
+            var flags = 0
+            if (appFlags and ApplicationInfo.FLAG_SYSTEM == 0) {
+                flags = flags or DOWNLOADED_FLAG
+                if (appFlags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP != 0) {
+                    flags = flags or UPDATED_SYSTEM_APP_FLAG
+                }
+            }
+            return flags
+        }
+
+        @JvmStatic
+        fun initFirstInstallTime(pi: PackageInfo): Long {
+            return pi.firstInstallTime
+        }
     }
 }
