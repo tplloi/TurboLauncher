f669cf68c59cbaa6b8d88e7a4fa8abb32e791a6f
loitp <www.muathu@gmail.com>
2023-04-29 09:56:17 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/DrawableStateProxyView.kt
+++ app/src/main/java/com/roy/turbo/launcher/DrawableStateProxyView.kt
@@ -1,53 +1,39 @@
-package com.roy.turbo.launcher;
+package com.roy.turbo.launcher
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.widget.LinearLayout
 
-public class DrawableStateProxyView extends LinearLayout {
+class DrawableStateProxyView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : LinearLayout(context, attrs, defStyle) {
+    private var mView: View? = null
+    private val mViewId: Int
 
-    private View mView;
-    private final int mViewId;
-
-    public DrawableStateProxyView(Context context) {
-        this(context, null);
+    init {
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.DrawableStateProxyView, defStyle, 0
+        )
+        mViewId = a.getResourceId(R.styleable.DrawableStateProxyView_sourceViewId, -1)
+        a.recycle()
+        isFocusable = false
     }
 
-    public DrawableStateProxyView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-
-    public DrawableStateProxyView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DrawableStateProxyView,
-                defStyle, 0);
-        mViewId = a.getResourceId(R.styleable.DrawableStateProxyView_sourceViewId, -1);
-        a.recycle();
-
-        setFocusable(false);
-    }
-
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-
+    override fun drawableStateChanged() {
+        super.drawableStateChanged()
         if (mView == null) {
-            View parent = (View) getParent();
-            mView = parent.findViewById(mViewId);
+            val parent = parent as View
+            mView = parent.findViewById(mViewId)
         }
-        if (mView != null) {
-            mView.setPressed(isPressed());
-            mView.setHovered(isHovered());
+        mView?.apply {
+            isPressed = isPressed
+            isHovered = isHovered
         }
     }
 
-    @Override
-    public boolean onHoverEvent(MotionEvent event) {
-        return false;
+    override fun onHoverEvent(event: MotionEvent): Boolean {
+        return false
     }
 }
