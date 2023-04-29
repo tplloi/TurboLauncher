d1657d11429bdbf04d11286a135e1c3f7af7a5c6
loitp <www.muathu@gmail.com>
2023-04-29 09:55:23 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/DrawableStateProxyView.java
+++ app/src/main/java/com/roy/turbo/launcher/DrawableStateProxyView.java
@@ -1,22 +1,4 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.roy.turbo.launcher;
-
-import com.roy.turbo.launcher.R;
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -28,7 +10,7 @@ import android.widget.LinearLayout;
 public class DrawableStateProxyView extends LinearLayout {
 
     private View mView;
-    private int mViewId;
+    private final int mViewId;
 
     public DrawableStateProxyView(Context context) {
         this(context, null);
