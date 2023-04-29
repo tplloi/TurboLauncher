699d18df0e058f770a366b76117e2f9e6bb844ac
loitp <www.muathu@gmail.com>
2023-04-29 16:03:33 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/LauncherAppWidgetInfo.java
+++ app/src/main/java/com/roy/turbo/launcher/LauncherAppWidgetInfo.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.roy.turbo.launcher;
 
 import android.appwidget.AppWidgetHostView;
@@ -25,16 +9,16 @@ import android.content.ContentValues;
  */
 class LauncherAppWidgetInfo extends ItemInfo {
 
-    /**
+    /*
      * Indicates that the widget hasn't been instantiated yet.
      */
-    static final int NO_ID = -1;
+//    static final int NO_ID = -1;
 
     /**
      * Identifier for this widget when talking with
      * {@link android.appwidget.AppWidgetManager} for updates.
      */
-    int appWidgetId = NO_ID;
+    int appWidgetId;
 
     ComponentName providerName;
 
@@ -88,7 +72,7 @@ class LauncherAppWidgetInfo extends ItemInfo {
 
     @Override
     public String toString() {
-        return "AppWidget(id=" + Integer.toString(appWidgetId) + ")";
+        return "AppWidget(id=" + appWidgetId + ")";
     }
 
     @Override
