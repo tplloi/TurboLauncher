9802be21d43a2bfad78e1e2f59b114f289aff418
loitp <www.muathu@gmail.com>
2023-04-29 19:07:15 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/OnAlarmListener.kt
+++ app/src/main/java/com/roy/turbo/launcher/OnAlarmListener.kt
@@ -1,7 +1,7 @@
-package com.roy.turbo.launcher;
+package com.roy.turbo.launcher
 
-import com.roy.turbo.launcher.sv.Alarm;
+import com.roy.turbo.launcher.sv.Alarm
 
-public interface OnAlarmListener {
-    public void onAlarm(Alarm alarm);
+interface OnAlarmListener {
+    fun onAlarm(alarm: Alarm?)
 }
