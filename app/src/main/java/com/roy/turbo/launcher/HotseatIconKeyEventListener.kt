4eccacd0e5328367c6edd762a4be7f2b7023f654
loitp <www.muathu@gmail.com>
2023-04-29 15:05:57 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/HotseatIconKeyEventListener.kt
+++ app/src/main/java/com/roy/turbo/launcher/HotseatIconKeyEventListener.kt
@@ -1,15 +1,14 @@
-package com.roy.turbo.launcher;
+package com.roy.turbo.launcher
 
-import android.content.res.Configuration;
-import android.view.KeyEvent;
-import android.view.View;
+import android.view.KeyEvent
+import android.view.View
 
 /**
  * A keyboard listener we set on all the hotseat buttons.
  */
-class HotseatIconKeyEventListener implements View.OnKeyListener {
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        final Configuration configuration = v.getResources().getConfiguration();
-        return FocusHelper.handleHotseatButtonKeyEvent(v, keyCode, event, configuration.orientation);
+internal class HotseatIconKeyEventListener : View.OnKeyListener {
+    override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+        val configuration = v.resources.configuration
+        return FocusHelper.handleHotseatButtonKeyEvent(v, keyCode, event, configuration.orientation)
     }
 }
