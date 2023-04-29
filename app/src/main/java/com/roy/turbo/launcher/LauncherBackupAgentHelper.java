bd7cd61e25242dfae5dc2e66de89816f32283b92
loitp <www.muathu@gmail.com>
2023-04-29 16:05:34 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/LauncherBackupAgentHelper.java
+++ app/src/main/java/com/roy/turbo/launcher/LauncherBackupAgentHelper.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.roy.turbo.launcher;
 
 import android.app.backup.BackupAgentHelper;
@@ -21,11 +5,10 @@ import android.app.backup.BackupManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.provider.Settings;
-import android.util.Log;
 
 public class LauncherBackupAgentHelper extends BackupAgentHelper {
 
-    private static final String TAG = "LauncherBackupAgentHelper";
+//    private static final String TAG = "LauncherBackupAgentHelper";
     static final boolean VERBOSE = true;
     static final boolean DEBUG = false;
 
@@ -59,14 +42,13 @@ public class LauncherBackupAgentHelper extends BackupAgentHelper {
 
     @Override
     public void onCreate() {
-        boolean restoreEnabled = 0 != Settings.Secure.getInt(
-                getContentResolver(), SETTING_RESTORE_ENABLED, 0);
-        if (VERBOSE) Log.v(TAG, "restore is " + (restoreEnabled ? "enabled" : "disabled"));
+        boolean restoreEnabled = 0 != Settings.Secure.getInt(getContentResolver(), SETTING_RESTORE_ENABLED, 0);
+//        if (VERBOSE) {
+//            Log.v(TAG, "restore is " + (restoreEnabled ? "enabled" : "disabled"));
+//        }
 
         addHelper(LauncherBackupHelper.LAUNCHER_PREFS_PREFIX,
-                new LauncherPreferencesBackupHelper(this,
-                        LauncherAppState.getSharedPreferencesKey(),
-                        restoreEnabled));
+                new LauncherPreferencesBackupHelper(this, LauncherAppState.getSharedPreferencesKey(), restoreEnabled));
         addHelper(LauncherBackupHelper.LAUNCHER_PREFIX,
                 new LauncherBackupHelper(this, restoreEnabled));
     }
