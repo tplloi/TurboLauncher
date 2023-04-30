fa7c0c86ea0aec08d44c2569af4e83bad306e169
loitp <www.muathu@gmail.com>
2023-04-30 10:26:38 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/ThirdPartyWallpaperPickerListAdapter.java
+++ app/src/main/java/com/roy/turbo/launcher/ThirdPartyWallpaperPickerListAdapter.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2010 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.roy.turbo.launcher;
 
 import android.content.ComponentName;
@@ -36,28 +20,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ThirdPartyWallpaperPickerListAdapter extends BaseAdapter implements ListAdapter {
-    private static final String LOG_TAG = "LiveWallpaperListAdapter";
 
     private final LayoutInflater mInflater;
     private final PackageManager mPackageManager;
     private final int mIconSize;
 
-    private List<ThirdPartyWallpaperTile> mThirdPartyWallpaperPickers =
-            new ArrayList<ThirdPartyWallpaperTile>();
+    private final List<ThirdPartyWallpaperTile> mThirdPartyWallpaperPickers = new ArrayList<>();
 
     public static class ThirdPartyWallpaperTile extends WallpaperPickerActivity.WallpaperTileInfo {
-        private ResolveInfo mResolveInfo;
+        private final ResolveInfo mResolveInfo;
         public ThirdPartyWallpaperTile(ResolveInfo resolveInfo) {
             mResolveInfo = resolveInfo;
         }
         @Override
         public void onClick(WallpaperPickerActivity a) {
-            final ComponentName itemComponentName = new ComponentName(
-                    mResolveInfo.activityInfo.packageName, mResolveInfo.activityInfo.name);
+            final ComponentName itemComponentName = new ComponentName(mResolveInfo.activityInfo.packageName, mResolveInfo.activityInfo.name);
             Intent launchIntent = new Intent(Intent.ACTION_SET_WALLPAPER);
             launchIntent.setComponent(itemComponentName);
-            a.startActivityForResultSafely(
-                    launchIntent, WallpaperPickerActivity.PICK_WALLPAPER_THIRD_PARTY_ACTIVITY);
+            a.startActivityForResultSafely(launchIntent, WallpaperPickerActivity.PICK_WALLPAPER_THIRD_PARTY_ACTIVITY);
         }
     }
 
@@ -68,14 +48,12 @@ public class ThirdPartyWallpaperPickerListAdapter extends BaseAdapter implements
         final PackageManager pm = mPackageManager;
 
         final Intent pickWallpaperIntent = new Intent(Intent.ACTION_SET_WALLPAPER);
-        final List<ResolveInfo> apps =
-                pm.queryIntentActivities(pickWallpaperIntent, 0);
+        final List<ResolveInfo> apps = pm.queryIntentActivities(pickWallpaperIntent, 0);
 
         // Get list of image picker intents
         Intent pickImageIntent = new Intent(Intent.ACTION_GET_CONTENT);
         pickImageIntent.setType("image/*");
-        final List<ResolveInfo> imagePickerActivities =
-                pm.queryIntentActivities(pickImageIntent, 0);
+        final List<ResolveInfo> imagePickerActivities = pm.queryIntentActivities(pickImageIntent, 0);
         final ComponentName[] imageActivities = new ComponentName[imagePickerActivities.size()];
         for (int i = 0; i < imagePickerActivities.size(); i++) {
             ActivityInfo activityInfo = imagePickerActivities.get(i).activityInfo;
