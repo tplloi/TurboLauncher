8eccb5872f3f12202acb59bd790daa754b5a06a6
loitp <www.muathu@gmail.com>
2023-04-30 16:26:30 +0700

    update

--- app/src/main/java/com/roy/LauncherApplication.kt
+++ app/src/main/java/com/roy/LauncherApplication.kt
@@ -6,6 +6,8 @@ import com.roy.turbo.launcher.LauncherAppState
 //TODO ic_launcher
 //TODO ad
 //TODO firebase
+//TODO rate, more app, share app
+//TODO policy
 class LauncherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
