9a379fe139857666bee0241c1a91e594d3a72971
loitp <www.muathu@gmail.com>
2023-04-30 10:08:13 +0700

    update

--- app/src/main/java/com/roy/turbo/launcher/PageIndicatorMarker.kt
+++ app/src/main/java/com/roy/turbo/launcher/PageIndicatorMarker.kt
@@ -1,85 +1,76 @@
-package com.roy.turbo.launcher;
+package com.roy.turbo.launcher
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.util.AttributeSet;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
 
-public class PageIndicatorMarker extends FrameLayout {
-    
-    private static final int MARKER_FADE_DURATION = 175;
+class PageIndicatorMarker @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : FrameLayout(
+    context, attrs, defStyle
+) {
+    private var mActiveMarker: ImageView? = null
+    private var mInactiveMarker: ImageView? = null
+    private var isActive = false
 
-    private ImageView mActiveMarker;
-    private ImageView mInactiveMarker;
-    private boolean mIsActive = false;
-
-    public PageIndicatorMarker(Context context) {
-        this(context, null);
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mActiveMarker = findViewById<View>(R.id.active) as ImageView
+        mInactiveMarker = findViewById<View>(R.id.inactive) as ImageView
     }
 
-    public PageIndicatorMarker(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+    fun setMarkerDrawables(activeResId: Int, inactiveResId: Int) {
+        val r = resources
+        mActiveMarker?.setImageDrawable(r.getDrawable(activeResId))
+        mInactiveMarker?.setImageDrawable(r.getDrawable(inactiveResId))
     }
 
-    public PageIndicatorMarker(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        mActiveMarker = (ImageView) findViewById(R.id.active);
-        mInactiveMarker = (ImageView) findViewById(R.id.inactive);
-    }
-
-    void setMarkerDrawables(int activeResId, int inactiveResId) {
-        Resources r = getResources();
-        mActiveMarker.setImageDrawable(r.getDrawable(activeResId));
-        mInactiveMarker.setImageDrawable(r.getDrawable(inactiveResId));
-    }
-
-    void activate(boolean immediate) {
+    fun activate(immediate: Boolean) {
         if (immediate) {
-            mActiveMarker.animate().cancel();
-            mActiveMarker.setAlpha(1f);
-            mActiveMarker.setScaleX(1f);
-            mActiveMarker.setScaleY(1f);
-            mInactiveMarker.animate().cancel();
-            mInactiveMarker.setAlpha(0f);
+            mActiveMarker?.apply {
+                animate().cancel()
+                alpha = 1f
+                scaleX = 1f
+                scaleY = 1f
+            }
+            mInactiveMarker?.apply {
+                animate().cancel()
+                alpha = 0f
+            }
         } else {
-            mActiveMarker.animate()
-                    .alpha(1f)
-                    .scaleX(1f)
-                    .scaleY(1f)
-                    .setDuration(MARKER_FADE_DURATION).start();
-            mInactiveMarker.animate()
-                    .alpha(0f)
-                    .setDuration(MARKER_FADE_DURATION).start();
+            mActiveMarker?.animate()?.alpha(1f)?.scaleX(1f)?.scaleY(1f)
+                ?.setDuration(MARKER_FADE_DURATION.toLong())?.start()
+
+            mInactiveMarker?.animate()?.alpha(0f)?.setDuration(MARKER_FADE_DURATION.toLong())
+                ?.start()
         }
-        mIsActive = true;
+        isActive = true
     }
 
-    void inactivate(boolean immediate) {
+    fun inactivate(immediate: Boolean) {
         if (immediate) {
-            mInactiveMarker.animate().cancel();
-            mInactiveMarker.setAlpha(1f);
-            mActiveMarker.animate().cancel();
-            mActiveMarker.setAlpha(0f);
-            mActiveMarker.setScaleX(0.5f);
-            mActiveMarker.setScaleY(0.5f);
+            mInactiveMarker?.apply {
+                animate().cancel()
+                alpha = 1f
+            }
+            mActiveMarker?.apply {
+                animate().cancel()
+                alpha = 0f
+                scaleX = 0.5f
+                scaleY = 0.5f
+            }
         } else {
-            mInactiveMarker.animate().alpha(1f)
-                    .setDuration(MARKER_FADE_DURATION).start();
-            mActiveMarker.animate()
-                    .alpha(0f)
-                    .scaleX(0.5f)
-                    .scaleY(0.5f)
-                    .setDuration(MARKER_FADE_DURATION).start();
+            mInactiveMarker?.animate()?.alpha(1f)?.setDuration(MARKER_FADE_DURATION.toLong())
+                ?.start()
+            mActiveMarker?.animate()?.alpha(0f)?.scaleX(0.5f)?.scaleY(0.5f)
+                ?.setDuration(MARKER_FADE_DURATION.toLong())?.start()
         }
-        mIsActive = false;
+        isActive = false
     }
 
-    boolean isActive() {
-        return mIsActive;
+    companion object {
+        private const val MARKER_FADE_DURATION = 175
     }
 }
