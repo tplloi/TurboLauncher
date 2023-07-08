925bd09d2a6970c2ade292a293e0b1666bda7245
ChauTriTin <freuss47@gmail.com>
2023-07-08 11:58:29 +0700

    update

---
+++ app/src/main/java/com/roy/ext/Applovin.kt
@@ -0,0 +1,144 @@
+package com.roy.ext
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.util.Log
+import android.util.Log.e
+import android.util.Log.i
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.applovin.mediation.MaxAd
+import com.applovin.mediation.MaxAdFormat
+import com.applovin.mediation.MaxAdViewAdListener
+import com.applovin.mediation.MaxError
+import com.applovin.mediation.ads.MaxAdView
+import com.applovin.sdk.AppLovinMediationProvider
+import com.applovin.sdk.AppLovinSdk
+import com.applovin.sdk.AppLovinSdkUtils
+import com.roy.turbo.launcher.BuildConfig
+import com.roy.turbo.launcher.R
+
+object Applovin {
+    fun setupAd(c: Context) {
+        // Please check config in gradle
+        // Please add key in manifest
+
+        // Initialize the AppLovin SDK
+        AppLovinSdk.getInstance(c).mediationProvider = AppLovinMediationProvider.MAX
+//        showMediationDebugger(c)
+        AppLovinSdk.getInstance(c).initializeSdk {
+            // AppLovin SDK is initialized, start loading ads now or later if ad gate is reached
+            e("Applovin", "setupAd initializeSdk $it")
+            if (BuildConfig.DEBUG) {
+                Toast.makeText(
+                    c,
+                    "Debug toast initializeSdk isTestModeEnabled: ${it.isTestModeEnabled}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    fun showMediationDebugger(c: Context) {
+        if (BuildConfig.DEBUG) {
+            AppLovinSdk.getInstance(c).showMediationDebugger()
+        } else {
+            Toast.makeText(
+                c,
+                "This feature is only available in debug mode",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    fun createAdBanner(
+        a: Activity,
+        logTag: String?,
+        bkgColor: Int = Color.RED,
+        viewGroup: ViewGroup,
+        isAdaptiveBanner: Boolean
+    ): MaxAdView {
+        val log = "$logTag - createAdBanner"
+        val enableAdBanner = a.getString(R.string.EnableAdBanner) == "true"
+        var id = "1234567890123456" // dummy id
+        if (enableAdBanner) {
+            id = a.getString(R.string.BANNER)
+            viewGroup.visibility = View.VISIBLE
+        } else {
+            viewGroup.visibility = View.GONE
+        }
+        i(log, "enableAdBanner $enableAdBanner -> $id")
+        val adView = MaxAdView(id, a)
+        adView.let { ad ->
+            ad.setListener(object : MaxAdViewAdListener {
+                override fun onAdLoaded(p0: MaxAd?) {
+                    i(log, "onAdLoaded")
+                    viewGroup.visibility = View.VISIBLE
+                }
+
+                override fun onAdDisplayed(p0: MaxAd?) {
+                    i(log, "onAdDisplayed")
+                }
+
+                override fun onAdHidden(p0: MaxAd?) {
+                    i(log, "onAdHidden")
+                }
+
+                override fun onAdClicked(p0: MaxAd?) {
+                    i(log, "onAdClicked")
+                }
+
+                override fun onAdLoadFailed(p0: String?, p1: MaxError?) {
+                    i(log, "onAdLoadFailed")
+                    viewGroup.visibility = View.GONE
+                }
+
+                override fun onAdDisplayFailed(p0: MaxAd?, p1: MaxError?) {
+                    i(log, "onAdDisplayFailed")
+                }
+
+                override fun onAdExpanded(p0: MaxAd?) {
+                    i(log, "onAdExpanded")
+                }
+
+                override fun onAdCollapsed(p0: MaxAd?) {
+                    i(log, "onAdCollapsed")
+                }
+
+            })
+            ad.setRevenueListener {
+                i(log, "onAdRevenuePaid")
+            }
+
+            if (isAdaptiveBanner) {
+                // Stretch to the width of the screen for banners to be fully functional
+                val width = ViewGroup.LayoutParams.MATCH_PARENT
+
+                // Get the adaptive banner height.
+                val heightDp = MaxAdFormat.BANNER.getAdaptiveSize(a).height
+                val heightPx = AppLovinSdkUtils.dpToPx(a, heightDp)
+
+                ad.layoutParams = FrameLayout.LayoutParams(width, heightPx)
+                ad.setExtraParameter("adaptive_banner", "true")
+                ad.setLocalExtraParameter("adaptive_banner_width", 400)
+                ad.adFormat.getAdaptiveSize(400, a).height // Set your ad height to this value
+            } else {
+                val isTablet = AppLovinSdkUtils.isTablet(a)
+                val heightPx = AppLovinSdkUtils.dpToPx(a, if (isTablet) 90 else 50)
+
+                ad.layoutParams = FrameLayout.LayoutParams(
+                    /* width = */ ViewGroup.LayoutParams.MATCH_PARENT,
+                    /* height = */ heightPx
+                )
+            }
+
+            ad.setBackgroundColor(bkgColor)
+            viewGroup.addView(adView)
+            ad.loadAd()
+        }
+        return adView
+    }
+}
--- app/src/main/java/com/roy/turbo/launcher/ThemeSettingsActivity.java
+++ app/src/main/java/com/roy/turbo/launcher/ThemeSettingsActivity.java
@@ -14,168 +14,194 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.applovin.mediation.ads.MaxAdView;
 import com.roy.ext.ActivityKt;
+import com.roy.ext.Applovin;
 import com.roy.ext.ContextKt;
 import com.roy.turbo.launcher.settings.SettingsProvider;
 import com.roy.turbo.launcher.ui.AFakeLauncher;
 
 import java.util.List;
 
-public class ThemeSettingsActivity  extends PreferenceActivity implements OnPreferenceChangeListener {
+public class ThemeSettingsActivity extends PreferenceActivity implements OnPreferenceChangeListener {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    private MaxAdView adView;
 
-		getPreferenceManager().setSharedPreferencesName(SettingsProvider.SETTINGS_KEY);
-		addPreferencesFromResource(R.xml.theme_settings);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		SharedPreferences sp = getPreferenceManager().getSharedPreferences();
-		final String themePackage = sp.getString("themePackageName", Launcher.THEME_DEFAULT);
-		ListPreference lp = (ListPreference) findPreference("themePackageName");
-		lp.setOnPreferenceChangeListener(this);
-		Intent intent = new Intent("com.roy.turbo.launcher.THEMES");
-		intent.addCategory("android.intent.category.DEFAULT");
-		PackageManager pm = getPackageManager();
-		List<ResolveInfo> themes = pm.queryIntentActivities(intent, 0);
-		String[] entries = new String[themes.size() + 1];
-		String[] values = new String[themes.size() + 1];
-		entries[0] = Launcher.THEME_DEFAULT;
-		values[0] = Launcher.THEME_DEFAULT;
-		for (int i = 0; i < themes.size(); i++) {
-			String appPackageName = (themes.get(i)).activityInfo.packageName;
-			String themeName = (themes.get(i)).loadLabel(pm).toString();
-			entries[i + 1] = themeName;
-			values[i + 1] = appPackageName;
-		}
-		lp.setEntries(entries);
-		lp.setEntryValues(values);
-		ThemePreference themePreview = (ThemePreference) findPreference("themePreview");
-		themePreview.setTheme(themePackage);
-	}
-    
-	public void applyTheme(View v) {
-		ThemePreference themePreview = (ThemePreference) findPreference("themePreview");
-		String packageName = themePreview.getValue().toString();
-		SharedPreferences sp = getPreferenceManager().getSharedPreferences();
-		SharedPreferences.Editor editor = sp.edit();
-		editor.putString("themePackageName", packageName);
+        getPreferenceManager().setSharedPreferencesName(SettingsProvider.SETTINGS_KEY);
+        addPreferencesFromResource(R.xml.theme_settings);
 
-		if (!packageName.equals(Launcher.THEME_DEFAULT)) {
-			Resources themeResources = null;
-			try {
-				themeResources = getPackageManager().getResourcesForApplication(packageName);
-			} catch (NameNotFoundException e) {
-				e.printStackTrace();
-			}
-			if (themeResources != null) {
-				int config_dockBgId = themeResources.getIdentifier("config_dockBg", "bool", packageName);
-				if (config_dockBgId != 0) {
-					boolean config_dockBg = themeResources.getBoolean(config_dockBgId);
-					editor.putBoolean("dockBg", config_dockBg);
-				}
-				int config_new_selectorsId = themeResources.getIdentifier("config_new_selectors", "bool", packageName);
-				if (config_new_selectorsId != 0) {
-					boolean config_new_selectors = themeResources.getBoolean(config_new_selectorsId);
-					editor.putBoolean("uiNewSelectors", config_new_selectors);
-				}
-				int config_drawerLabelsId = themeResources.getIdentifier("config_drawerLabels", "bool", packageName);
-				if (config_drawerLabelsId != 0) {
-					boolean config_drawerLabels = themeResources.getBoolean(config_drawerLabelsId);
-					editor.putBoolean("drawerLabels", config_drawerLabels);
-				}
-				int config_fadeDrawerLabelsId = themeResources.getIdentifier("config_fadeDrawerLabels", "bool", packageName);
-				if (config_fadeDrawerLabelsId != 0) {
-					boolean config_fadeDrawerLabels = themeResources.getBoolean(config_fadeDrawerLabelsId);
-					editor.putBoolean("fadeDrawerLabels", config_fadeDrawerLabels);
-				}
-				int config_page_indicatorId = themeResources.getIdentifier("config_page_indicator", "bool", packageName);
-				if (config_page_indicatorId != 0) {
-					boolean config_page_indicator = themeResources.getBoolean(config_page_indicatorId);
-					editor.putBoolean("pageIndicator", config_page_indicator);
-				}
-				int config_selectors_colorId = themeResources.getIdentifier("config_selectors_color", "integer", packageName);
-				if (config_selectors_colorId != 0) {
-					int config_selectors_color = themeResources.getInteger(config_selectors_colorId);
-					editor.putInt("selectors_color", config_selectors_color);
-				}
-				int config_selectors_color_focusId = themeResources.getIdentifier("config_selectors_color_focus", "integer", packageName);
-				if (config_selectors_color_focusId != 0) {
-					int config_selectors_color_focus = themeResources.getInteger(config_selectors_color_focusId);
-					editor.putInt("selectors_color_focus", config_selectors_color_focus);
-				}
-				int config_drawer_colorId = themeResources.getIdentifier(
-						"config_drawer_color", "integer",
-						packageName);
-				if (config_drawer_colorId != 0) {
-					int config_drawer_color = themeResources.getInteger(config_drawer_colorId);
-					editor.putInt("drawer_color", config_drawer_color);
-				}
-				int config_page_indicator_typeId = themeResources.getIdentifier("config_page_indicator_type", "string", packageName);
-				if (config_page_indicator_typeId != 0) {
-					String config_page_indicator_type = themeResources
-							.getString(config_page_indicator_typeId);
-					editor.putString("pageIndicatorType",
-							config_page_indicator_type);
-				}
-				int config_ab_scale_factorId = themeResources.getIdentifier(
-						"config_ab_scale_factor", "integer",
-						packageName);
-				if (config_ab_scale_factorId != 0) {
-					int config_ab_scale_factor = themeResources
-							.getInteger(config_ab_scale_factorId);
-					editor.putInt("iconScale", config_ab_scale_factor);
-				}
-				int dock_styleId = themeResources.getIdentifier(
-						"main_dock_style", "string", packageName);
-				if (dock_styleId != 0) {
-					String dock_style = themeResources.getString(dock_styleId);
-					editor.putString("main_dock_style", dock_style);
+        SharedPreferences sp = getPreferenceManager().getSharedPreferences();
+        final String themePackage = sp.getString("themePackageName", Launcher.THEME_DEFAULT);
+        ListPreference lp = (ListPreference) findPreference("themePackageName");
+        lp.setOnPreferenceChangeListener(this);
+        Intent intent = new Intent("com.roy.turbo.launcher.THEMES");
+        intent.addCategory("android.intent.category.DEFAULT");
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> themes = pm.queryIntentActivities(intent, 0);
+        String[] entries = new String[themes.size() + 1];
+        String[] values = new String[themes.size() + 1];
+        entries[0] = Launcher.THEME_DEFAULT;
+        values[0] = Launcher.THEME_DEFAULT;
+        for (int i = 0; i < themes.size(); i++) {
+            String appPackageName = (themes.get(i)).activityInfo.packageName;
+            String themeName = (themes.get(i)).loadLabel(pm).toString();
+            entries[i + 1] = themeName;
+            values[i + 1] = appPackageName;
+        }
+        lp.setEntries(entries);
+        lp.setEntryValues(values);
+        ThemePreference themePreview = (ThemePreference) findPreference("themePreview");
+        themePreview.setTheme(themePackage);
+        themePreview.setCallBackFlAd(new ThemePreference.CallBackFlAd() {
+            @Override
+            public void onFlReady(FrameLayout flAd) {
+                adView = Applovin.INSTANCE.createAdBanner(
+                        ThemeSettingsActivity.this,
+                        ThemeSettingsActivity.class.getSimpleName(),
+                        Color.TRANSPARENT,
+                        flAd,
+                        true
+                );
+            }
+        });
+    }
 
-				}
-				// TODO:TURBO We set the theme wallpaper. We should add this as
-				// optional...
-				int wallpaperId = themeResources.getIdentifier(
-						"theme_wallpaper", "drawable", packageName);
-				if (wallpaperId != 0) {
-					Options mOptions = new BitmapFactory.Options();
-					mOptions.inDither = false;
-					mOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
-					Bitmap wallpaper = null;
-					try {
-						wallpaper = BitmapFactory.decodeResource(themeResources, wallpaperId, mOptions);
-					} catch (OutOfMemoryError e) {
-						e.printStackTrace();
-					}
-					if (wallpaper != null) {
-						try {
-							WallpaperManager wpm = (WallpaperManager) getSystemService(WALLPAPER_SERVICE);
-							// wpm.setResource(mImages.get(position));
-							wpm.setBitmap(wallpaper);
-							wallpaper.recycle();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
+    public void applyTheme(View v) {
+        ThemePreference themePreview = (ThemePreference) findPreference("themePreview");
+        String packageName = themePreview.getValue().toString();
+        SharedPreferences sp = getPreferenceManager().getSharedPreferences();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("themePackageName", packageName);
 
-			}
-		}
+        if (!packageName.equals(Launcher.THEME_DEFAULT)) {
+            Resources themeResources = null;
+            try {
+                themeResources = getPackageManager().getResourcesForApplication(packageName);
+            } catch (NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (themeResources != null) {
+                int config_dockBgId = themeResources.getIdentifier("config_dockBg", "bool", packageName);
+                if (config_dockBgId != 0) {
+                    boolean config_dockBg = themeResources.getBoolean(config_dockBgId);
+                    editor.putBoolean("dockBg", config_dockBg);
+                }
+                int config_new_selectorsId = themeResources.getIdentifier("config_new_selectors", "bool", packageName);
+                if (config_new_selectorsId != 0) {
+                    boolean config_new_selectors = themeResources.getBoolean(config_new_selectorsId);
+                    editor.putBoolean("uiNewSelectors", config_new_selectors);
+                }
+                int config_drawerLabelsId = themeResources.getIdentifier("config_drawerLabels", "bool", packageName);
+                if (config_drawerLabelsId != 0) {
+                    boolean config_drawerLabels = themeResources.getBoolean(config_drawerLabelsId);
+                    editor.putBoolean("drawerLabels", config_drawerLabels);
+                }
+                int config_fadeDrawerLabelsId = themeResources.getIdentifier("config_fadeDrawerLabels", "bool", packageName);
+                if (config_fadeDrawerLabelsId != 0) {
+                    boolean config_fadeDrawerLabels = themeResources.getBoolean(config_fadeDrawerLabelsId);
+                    editor.putBoolean("fadeDrawerLabels", config_fadeDrawerLabels);
+                }
+                int config_page_indicatorId = themeResources.getIdentifier("config_page_indicator", "bool", packageName);
+                if (config_page_indicatorId != 0) {
+                    boolean config_page_indicator = themeResources.getBoolean(config_page_indicatorId);
+                    editor.putBoolean("pageIndicator", config_page_indicator);
+                }
+                int config_selectors_colorId = themeResources.getIdentifier("config_selectors_color", "integer", packageName);
+                if (config_selectors_colorId != 0) {
+                    int config_selectors_color = themeResources.getInteger(config_selectors_colorId);
+                    editor.putInt("selectors_color", config_selectors_color);
+                }
+                int config_selectors_color_focusId = themeResources.getIdentifier("config_selectors_color_focus", "integer", packageName);
+                if (config_selectors_color_focusId != 0) {
+                    int config_selectors_color_focus = themeResources.getInteger(config_selectors_color_focusId);
+                    editor.putInt("selectors_color_focus", config_selectors_color_focus);
+                }
+                int config_drawer_colorId = themeResources.getIdentifier(
+                        "config_drawer_color", "integer",
+                        packageName);
+                if (config_drawer_colorId != 0) {
+                    int config_drawer_color = themeResources.getInteger(config_drawer_colorId);
+                    editor.putInt("drawer_color", config_drawer_color);
+                }
+                int config_page_indicator_typeId = themeResources.getIdentifier("config_page_indicator_type", "string", packageName);
+                if (config_page_indicator_typeId != 0) {
+                    String config_page_indicator_type = themeResources
+                            .getString(config_page_indicator_typeId);
+                    editor.putString("pageIndicatorType",
+                            config_page_indicator_type);
+                }
+                int config_ab_scale_factorId = themeResources.getIdentifier(
+                        "config_ab_scale_factor", "integer",
+                        packageName);
+                if (config_ab_scale_factorId != 0) {
+                    int config_ab_scale_factor = themeResources
+                            .getInteger(config_ab_scale_factorId);
+                    editor.putInt("iconScale", config_ab_scale_factor);
+                }
+                int dock_styleId = themeResources.getIdentifier(
+                        "main_dock_style", "string", packageName);
+                if (dock_styleId != 0) {
+                    String dock_style = themeResources.getString(dock_styleId);
+                    editor.putString("main_dock_style", dock_style);
 
-		editor.apply();
-		finish();
-	}
-	
-	public void getThemes(View v) {
-		searchIconPack(this);
+                }
+                // TODO:TURBO We set the theme wallpaper. We should add this as
+                // optional...
+                int wallpaperId = themeResources.getIdentifier(
+                        "theme_wallpaper", "drawable", packageName);
+                if (wallpaperId != 0) {
+                    Options mOptions = new BitmapFactory.Options();
+                    mOptions.inDither = false;
+                    mOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    Bitmap wallpaper = null;
+                    try {
+                        wallpaper = BitmapFactory.decodeResource(themeResources, wallpaperId, mOptions);
+                    } catch (OutOfMemoryError e) {
+                        e.printStackTrace();
+                    }
+                    if (wallpaper != null) {
+                        try {
+                            WallpaperManager wpm = (WallpaperManager) getSystemService(WALLPAPER_SERVICE);
+                            // wpm.setResource(mImages.get(position));
+                            wpm.setBitmap(wallpaper);
+                            wallpaper.recycle();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }
+        }
+
+        editor.apply();
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (adView != null) {
+            adView.destroy();
+        }
+    }
+
+    public void getThemes(View v) {
+        searchIconPack(this);
 //		Uri marketUri = Uri.parse("market://search?q=Turbo Launcher Theme");
 //		Intent marketIntent = new Intent(Intent.ACTION_VIEW).setData(marketUri);
 //		try {
@@ -183,41 +209,41 @@ public class ThemeSettingsActivity  extends PreferenceActivity implements OnPref
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
-		finish();
-	}
+        finish();
+    }
 
-	public void setDefaultLauncher(View v) {
-		if (isDefaultLauncher(this)) {
-			Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
-		} else {
-			chooseLauncher(this, AFakeLauncher.class);
-		}
-	}
+    public void setDefaultLauncher(View v) {
+        if (isDefaultLauncher(this)) {
+            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+        } else {
+            chooseLauncher(this, AFakeLauncher.class);
+        }
+    }
 
-	public void rateApp(View v) {
-		ActivityKt.rateApp(this, getPackageName());
-	}
+    public void rateApp(View v) {
+        ActivityKt.rateApp(this, getPackageName());
+    }
 
-	public void moreApp(View v) {
-		ActivityKt.moreApp(this, "Roy93Group");
-	}
+    public void moreApp(View v) {
+        ActivityKt.moreApp(this, "Roy93Group");
+    }
 
-	public void shareApp(View v) {
-		ActivityKt.shareApp(this);
-	}
+    public void shareApp(View v) {
+        ActivityKt.shareApp(this);
+    }
 
-	public void policyApp(View v) {
-		ContextKt.openBrowserPolicy(this);
-	}
+    public void policyApp(View v) {
+        ContextKt.openBrowserPolicy(this);
+    }
 
-	@Override
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-		if (preference.getKey().equals("themePackageName")) {
-			ThemePreference themePreview = (ThemePreference) findPreference("themePreview");
-			themePreview.setTheme(newValue.toString());
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.getKey().equals("themePackageName")) {
+            ThemePreference themePreview = (ThemePreference) findPreference("themePreview");
+            themePreview.setTheme(newValue.toString());
+            return false;
+        }
+        return true;
+    }
 
 }
